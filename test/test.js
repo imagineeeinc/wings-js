11abeb0c9e20948ffad24f85299c36729cc2c68e
Imagineee <imagineeelego@gmail.com>
2021-06-02 12:48:35 +0100

    added serving files

--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ var app = new wings.server()
 
 app.listen(3000, () => console.log("running on port 3000"))
 
-app.onGet("/", () => {
-    console.log("requesting '/'")
+app.use('files', './public')
+
+app.onGet("/", (ctx) => {
+    ctx.res.send("hi")
+})
+app.onGet("/html", (ctx) => {
+    ctx.res.sendFile('./public/index.html')
 })
--- wing.js
+++ wing.js
@@ -1,15 +1,25 @@
 const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
 
 class server {
     constructor() {
         this._stack = []
+        this.loc = process.cwd()
+    }
+    use(type, ...args) {
+        if (type === 'files') {
+            this.files = path.join(this.loc, args[0])
+            console.log(this.files)
+        }
     }
     listen(port, callback) {
         const handler = (req, res) => {
             this.handle(req, res, err => {
                 if (err) {
-                    res.writeHead(500);
-                    res.end('Internal Server Error');
+                    res.writeHead(500)
+                    res.end('Internal Server Error')
                 }
             })
         }
@@ -17,12 +27,55 @@ class server {
     }
     onGet(loc, callback) {
         this._stack.push({loc: loc, callback: callback})
-        console.log(this._stack)
     }
     handle(req, res, callback) {
-        console.log(req.path)
-        //res.setHeader("Content-Type", "text/csv");
-        //res.setHeader("Content-Disposition", "attachment;filename=oceanpals.csv")
+        for (var i=0;i < this._stack.length;i++) {
+            if (req.url === this._stack[i].loc) {
+                res.statusCode = 200
+                res.setHeader('Content-Type', 'text/html')
+                res = customRes(res, this)
+                let ctx = {req: req, res: res}
+                return this._stack[i].callback(ctx)
+            } else {
+                console.log(req.url)
+                fs.readdir(this.loc, (err, files) => {
+                    if (err) {
+                        console.error(err)
+                    }
+                    let di = files
+                    for (var l=0;l < di.length;l++) {
+                        if (req.url === di[l]) {
+                            //res.setHeader('Content-Type', 'text/html')
+                            //res = customRes(res, this)
+                            res.statusCode = 200
+                            fs.readFile(th.loc + "/" + di[l], function(err, data) {
+                                if (err) {
+                                    console.error(err)
+                                }
+                                t.send(data)
+                            })
+                        } else {
+                            res.statusCode = 400
+                        }
+                    }
+                })
+            }
+        }
+        function customRes(res, th) {
+            res.send = function (data) {
+                this.end(data)
+            }
+            res.sendFile = function (file) {
+                let t = this
+                fs.readFile(th.loc + "/" + file, function(err, data) {
+                    if (err) {
+                        console.error(err)
+                    }
+                    t.send(data)
+                })
+            }
+            return res
+        }
     }
 }
 
